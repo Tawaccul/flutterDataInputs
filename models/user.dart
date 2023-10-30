2aad3a790fbafb481191fe73fd192ae2b4fcddea
Tawaccul <teps2222@gmail.com>
2023-10-30 21:05:46 +0100

    commit

---
+++ models/user.dart
@@ -0,0 +1,7 @@
+class User {
+  late String name;
+  late String phone;
+  late String email;
+  late String country;
+  late String story;
+}
---
+++ pages/user_info_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+
+class UserInfoPage extends StatelessWidget {
+  final User userInfo;
+  const UserInfoPage({Key? key, required this.userInfo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Info'),
+        centerTitle: true,
+      ),
+      body: Card(
+        margin: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Visibility(
+              visible: userInfo.email.isEmpty ? false : true,
+              child: ListTile(
+                title: Text(
+                  userInfo.name,
+                  style: const TextStyle(fontWeight: FontWeight.w500),
+                ),
+                subtitle: Text(userInfo.story),
+                leading: const Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+                trailing: Text(userInfo.country),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                userInfo.phone,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: const Icon(
+                Icons.phone,
+                color: Colors.black,
+              ),
+            ),
+            Visibility(
+              visible: userInfo.email.isEmpty ? false : true,
+              child: ListTile(
+                title: Text(
+                  userInfo.email.isEmpty ? 'Not specified' : userInfo.email,
+                  style: const TextStyle(fontWeight: FontWeight.w500),
+                ),
+                leading: const Icon(
+                  Icons.mail,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
